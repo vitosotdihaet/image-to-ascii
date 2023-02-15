732fc354279ab505c0f26f5f2aac198af9a91dd6
vitosotdihaet <vitostikitos@gmail.com>
2023-02-15 15:18:21 +0300

    Make this project CLI

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 from PIL import Image
-import os
+from pathlib import Path
+from time import time
 
-if not os.path.exists('i.jpg'):
-    print(f'i.jpg does not exist in {os.getcwd()}!')
-    print(f'Please check file\'s extension!')
-    exit()
+import argparse
 
-f = open('o.txt', 'w')
+DEFAULT_COMPRESSION = 0.5
+# offset x for saving resolution
+DEFAULT_RATIO = 2.35
 
-# inverse = True if terminal background is a bright color
-inverse = False
+parser = argparse.ArgumentParser()
+
+parser.add_argument("path")
+parser.add_argument("-c", "--compression", default=DEFAULT_COMPRESSION, type=float)
+parser.add_argument("-s", "--save", action="store_true")
+parser.add_argument("-b", "--inverse", action="store_true")
+parser.add_argument("-r", "--ratio", default=DEFAULT_RATIO, type=float)
+
+args = parser.parse_args()
+
+target_img = Path(args.path)
+
+if not target_img.is_file():
+    print("The target image doesn't exist!")
+    exit(1)
+
+# inverse_brightness = True if terminal background is a bright color
+inverse_brightness = args.inverse
 
 # terminal_output = True if you want to print the result to terminal
-terminal_output = False
+terminal_output = not args.save
+
+compression = args.compression
+
+if not terminal_output: f = open(f'out-{time()}.txt', 'w')
 
 brightness = '8@#DZL]waxv?1(/|=+*":_-,.`  '[::-1]
-if inverse:
+if inverse_brightness:
     brightness = brightness[::-1]
 
 l = len(brightness)
-compression = 0.5
 
-image = Image.open('i.jpg')
+image = Image.open(target_img)
 x, y = image.size
 
-# offset x for saving resolution
-x, y = int(100 * compression * 2.35), int(y/(x/100) * compression)
+x, y = int(100 * compression * args.ratio), int(y/(x/100) * compression)
 
 image = image.resize((x, y))
 
@@ -35,12 +53,13 @@ for r in range(y):
     for c in range(x):
         colors = pixels.getpixel((c, r))
         gray = int(colors[0] * 0.3 + colors[1] * 0.5 + colors[2] * 0.2)
-        cp = brightness[int(l * (gray - 1)/256)]
-        f.write(cp)
-        if terminal_output:
-            print(cp, end='')
-    f.write('\n')
-    if terminal_output:
-        print()
 
-f.close()
+        cp = brightness[int(l * (gray - 1)/256)]
+
+        if terminal_output: print(cp, end='')
+        else: f.write(cp)
+
+    if terminal_output: print()
+    else: f.write('\n')
+
+if not terminal_output: f.close()
